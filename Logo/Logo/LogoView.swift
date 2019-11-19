2affcff88c56f018b648d4f9c199a12c85e81b52
Blake Price <blakeandrewprice@gmail.com>
2019-11-19 13:09:58 -0800

    Main Rectangle!

--- Logo/Logo/LogoView.swift
+++ Logo/Logo/LogoView.swift
@@ -8,8 +8,34 @@
 
 import UIKit
 
+@IBDesignable
 class LogoView: UIView {
+    private let lambdaRed = UIColor.red
+    private let lambdaWhite = UIColor.white
+
+    private let heightOffSet: CGFloat = 70.0
+    private let logoHeightSize: CGFloat = 30.0
+    private let logoWidthSize: CGFloat = 10.0
+
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        if let context = UIGraphicsGetCurrentContext() {
+            let mainRect = CGRect(x: rect.origin.x,
+                                   y: rect.origin.y,
+                                   width: rect.size.width,
+                                   height: rect.size.height - heightOffSet)
+            context.beginPath()
+            let mainRectPath = CGPath(roundedRect: mainRect,
+                                         cornerWidth: 0.0,
+                                         cornerHeight: 0.0,
+                                         transform: nil)
+            
+            context.addPath(mainRectPath)
+            context.setFillColor(lambdaRed.cgColor)
+            context.fillPath()
+            context.closePath()
+            
+            let squareCenter = CGPoint(x: rect.size.width / 2.0,
+                                       y: (rect.size.height - heightOffSet) / 2.0)
+        }
     }
 }
