e67b11f10a15e829ef18fb7c7e658e2c9f7e8a1d
Blake Price <blakeandrewprice@gmail.com>
2019-11-19 11:22:20 -0800

    Start!

---
+++ Logo/Logo/LogoView.swift
@@ -0,0 +1,15 @@
+//
+//  LogoView.swift
+//  Logo
+//
+//  Created by Blake Andrew Price on 11/19/19.
+//  Copyright © 2019 Blake Andrew Price. All rights reserved.
+//
+
+import UIKit
+
+class LogoView: UIView {
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+}
---
+++ Logo/Logo/LogoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LogoViewController.swift
+//  Logo
+//
+//  Created by Blake Andrew Price on 11/19/19.
+//  Copyright © 2019 Blake Andrew Price. All rights reserved.
+//
+
+import UIKit
+
+class LogoViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
